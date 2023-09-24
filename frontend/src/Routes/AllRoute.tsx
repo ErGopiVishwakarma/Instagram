b92fe52663603ace157c6ef9029c1f475c6e6b1d
Gopi vishwakarma <g2468vi@gmail.com>
2023-09-24 10:12:08 +0530

    rotue page added

---
+++ frontend/src/Routes/AllRoute.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../Pages/Home';
+import Message from '../Pages/Message';
+import Profile from '../Pages/Profile';
+import Login from '../Pages/Login';
+import Signup from '../Pages/Signup';
+import ProtectedRoute from './ProtectedRoute';
+
+const AllRoute = () => {
+  return (
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/message'
+        element={
+          <ProtectedRoute>
+            <Message />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  );
+};
+
+export default AllRoute;
