e31bc3b20e7960037e91fafaa71fb789580088f2
Gopi vishwakarma <g2468vi@gmail.com>
2023-09-24 09:55:27 +0530

    initial

---
+++ frontend/src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@material-tailwind/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './Redux/store';
+import ContextProvider from './Routes/ContextProvider';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <ThemeProvider>
+        <ContextProvider>
+          <App />
+        </ContextProvider>
+      </ThemeProvider>
+    </Provider>
+  </BrowserRouter>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
