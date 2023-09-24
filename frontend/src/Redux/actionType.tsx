46eb7fcf7d94b1c1b6fd91a60c845e78a598f902
Gopi vishwakarma <g2468vi@gmail.com>
2023-09-24 10:10:05 +0530

    redux actionType page added

---
+++ frontend/src/Redux/actionType.tsx
@@ -0,0 +1,7 @@
+export const AUTH: string = 'AUTH';
+export const AUTHUSER: string = 'AUTHUSER';
+export const ALLUSER: string = 'ALLUSER'
+export const ALLCHATS: string = 'ALLCHATS';
+export const ALLMESSAGE: string = 'ALLMESSAGE';
+export const POST: string = 'POST';
+export const UPDATEMESSAGE: string = 'UPDATEMESSAGE'
