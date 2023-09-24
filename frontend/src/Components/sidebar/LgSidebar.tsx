409bfef3d881278992c2017ba55a2bb53bf1967e
Gopi vishwakarma <g2468vi@gmail.com>
2023-09-24 12:17:10 +0530

    lg sidebar added

---
+++ frontend/src/Components/sidebar/LgSidebar.tsx
@@ -0,0 +1,93 @@
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  Avatar,
+} from '@material-tailwind/react';
+import { BsSearch } from 'react-icons/bs';
+import { GoVideo } from 'react-icons/go';
+import { PiTelegramLogoBold } from 'react-icons/pi';
+import { MdOutlineExplore } from 'react-icons/md';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { GrAddCircle } from 'react-icons/gr';
+import { FiMenu } from 'react-icons/fi';
+import { BiSolidHome, BiSolidShoppingBagAlt } from 'react-icons/bi';
+import { IconType } from 'react-icons/lib/esm/iconBase';
+import SearchDrawer from './SearchDrawer';
+import { NavLink } from 'react-router-dom';
+
+export default function LgSidebar() {
+  return (
+    <Card className='h-[calc(100vh)] w-full max-w-[15.5rem] rounded-none py-4 px-2 shadow-xl shadow-blue-gray-900/5 border-r border-gray-400 sticky top-0 '>
+      <div className='mb-2 p-4'>
+        <Typography variant='h4' color='blue-gray'>
+          Instagram
+        </Typography>
+      </div>
+      <List className='gap-2'>
+        <NavLink to='/'>
+          <ListItem>
+            <ListItemPrefix>
+              <BiSolidHome className='h-6 w-6' />
+            </ListItemPrefix>
+            Home
+          </ListItem>
+        </NavLink>
+        <SearchDrawer>
+          <ListItem>
+            <ListItemPrefix>
+              <BsSearch className='h-6 w-6' />
+            </ListItemPrefix>
+            Search
+          </ListItem>
+        </SearchDrawer>
+        <ListItem>
+          <ListItemPrefix>
+            <MdOutlineExplore className='h-6 w-6' />
+          </ListItemPrefix>
+        Explore
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <GoVideo className='h-6 w-6' />
+          </ListItemPrefix>
+          Reels
+        </ListItem>
+        <NavLink to='/message'>
+          <ListItem>
+            <ListItemPrefix>
+              <PiTelegramLogoBold className='h-6 w-6' />
+            </ListItemPrefix>
+            Message
+          </ListItem>
+        </NavLink>
+        <ListItem>
+          <ListItemPrefix>
+            <AiOutlineHeart className='h-6 w-6' />
+          </ListItemPrefix>
+          Notification
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <GrAddCircle className='h-6 w-6' />
+          </ListItemPrefix>
+          Create
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <Avatar className='h-6 w-6' />
+          </ListItemPrefix>
+          Profile
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <FiMenu className='h-6 w-6' />
+          </ListItemPrefix>
+          More
+        </ListItem>
+      </List>
+    </Card>
+  );
+}
