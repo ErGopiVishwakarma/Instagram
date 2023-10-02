426bb8c97ab43b01aeffaffa2cee20da03c16178
Gopi vishwakarma <g2468vi@gmail.com>
2023-10-03 05:00:19 +0530

    code added

--- frontend/src/Pages/Creates.tsx
+++ frontend/src/Pages/Creates.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { ReactNode, useState, useContext, useRef } from 'react';
+import { Dialog, DialogBody } from '@material-tailwind/react';
+import { GrClose } from 'react-icons/gr';
+import popupImage from '../Images/postPopupImage.png';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
+import { MdOutlineRectangle, MdPermMedia } from 'react-icons/md';
+import { BiCrop, BiImageAlt } from 'react-icons/bi';
+import { BsArrowLeftCircle, BsSquare } from 'react-icons/bs';
 
-const Creates = () => {
-  return (
-    <div>Creates</div>
-  )
+interface Children {
+  children: ReactNode;
 }
 
-export default Creates
+export default function Create({ children }: Children) {
+  const [open, setOpen] = useState<boolean>(false);
+  const handleOpen = () => setOpen(!open);
+  const cropperRef: any = useRef(null);
+    </>
+  );
+}
