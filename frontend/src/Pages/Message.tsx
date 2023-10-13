8a6d2eb86a494a300f4a374495997848b4caa8ea
Gopi vishwakarma <g2468vi@gmail.com>
2023-10-13 09:27:59 +0530

    message page update

--- frontend/src/Pages/Message.tsx
+++ frontend/src/Pages/Message.tsx
@@ -11,49 +11,22 @@ import { Initial } from '../Types/reducerType';
 import { getAllMessages } from '../Redux/action';
 import { ChatType, MessageType } from '../Types/otherType';
 import { UPDATEMESSAGE } from '../Redux/actionType';
+import MdSidebar from '../Components/sidebar/MdSidebar';
 
 // const backEndPoint = 'http://localhost:8080';
 // var socket: any, chatCompare: ChatType;
 
 const Message = () => {
   const { selectedChat, setSelectedChat } = useContext(Context) as ContextType;
-  // const authUser: any = useSelector((store: Initial) => store.authUser);
-  // const user: any = selectedChat;
-  // const dispatch = useDispatch();
-  // const data = useSelector((store: Initial) => store.localStorageData);
-  // useEffect(() => {
-  //   socket = io(backEndPoint);
-  //   socket.emit('roomSetup', authUser);
-  //   socket.on('connected', () => {});
-  //   socket.emit('joinChatPage', user._id);
-  // }, [selectedChat]);
-
-  // useEffect(() => {
-  //   getAllMessages(data.token, user._id, dispatch);
-  //   chatCompare = user;
-  // }, [selectedChat]);
-
-  // useEffect(() => {
-  //   socket.on('recieveMessage', (returnMessage: MessageType) => {
-  //     if (!chatCompare || user._id !== returnMessage._id) {
-  //       //give notification
-  //     } else {
-  //       console.log('recieve mesthod dispach')
-  //       dispatch({ type: UPDATEMESSAGE, payload: returnMessage });
-  //     }
-  //   });
-  // });
-
-  // const a = useSelector((store: Initial) => console.log(store));
 
   return (
-    <div className='w-full h-[100vh] flex  '>
+    <div className='w-full h-[100vh] flex '>
       {/* left side bar code  */}
-      <div className='flex'>
-        <div className='hidden md:block'>
-          <IconSidebar />
+      <div className='flex relative'>
+        <div className='hidden md:block relative z-50'>
+          <MdSidebar />
         </div>
-        <div className={`${!selectedChat ? 'block' : 'hidden'} md:block`}>
+        <div className={`${!selectedChat ? 'block' : 'hidden'} md:block relative z-20`}>
           <UserList />
         </div>
       </div>
