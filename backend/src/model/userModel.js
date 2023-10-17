d072c25021fb8c3feff53e128e50c5bfc2fb64d6
Gopi vishwakarma <g2468vi@gmail.com>
2023-10-17 14:35:28 +0530

    usermodel page update

--- backend/src/model/userModel.js
+++ backend/src/model/userModel.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     name:{type:String,default:'' , required:true},
     email:{type:String,default:'' , required:true},
+    highlights:{type:String,default:''},
     password:{type:String,default:'' , required:true},
     username:{type:String,default:'' , required:true},
     followers:[{type:mongoose.Schema.Types.ObjectId,ref:'user'}],
