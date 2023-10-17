e219d9a4063db57a2f017a5e63ee05e3b2efe2f5
Gopi vishwakarma <g2468vi@gmail.com>
2023-10-17 14:44:44 +0530

    code update

--- frontend/src/Components/profile/PanalSection/Posts.tsx
+++ frontend/src/Components/profile/PanalSection/Posts.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { BsHeartFill } from 'react-icons/bs';
 import { FaRegComment } from 'react-icons/fa';
 import {useState} from 'react'
+import { PostType } from '../../../Types/otherType';
 
-interface PostType {
-    el:string
+interface PostTypes {
+    el:PostType
 }
 
-const Posts = ({el}:PostType) => {
+const Posts = ({el}:PostTypes) => {
     const [show, setShow] = useState<boolean>(false)
   return (
     <div className='relative cursor-pointer' onMouseOver={()=>setShow(true)} onMouseOut={()=>setShow(false)}>
       <img
-        src={el}
+        src={`http://localhost:8080/${el?.postUrl}`}
         className=' h-full w-full'
       />
       <div className={`h-full w-full absolute top-0 hover:bg-[#0a0a0a4d] z-20 ${show?'flex':'hidden'} justify-center items-center gap-3 cursor-pointer`}>
