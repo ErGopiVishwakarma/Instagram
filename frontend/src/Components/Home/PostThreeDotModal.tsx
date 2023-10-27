34bd829cc1c76dcd5234789c9fa44407871a1ba1
Gopi vishwakarma <g2468vi@gmail.com>
2023-10-27 13:44:08 +0530

    Component update

--- frontend/src/Components/Home/PostThreeDotModal.tsx
+++ frontend/src/Components/Home/PostThreeDotModal.tsx
@@ -1,15 +1,22 @@
 import React, { ReactNode, useState } from 'react';
 import { Avatar, Dialog, DialogBody } from '@material-tailwind/react';
-import { Initial } from '../../Types/reducerType';
+import { AuthUser, Initial } from '../../Types/reducerType';
 import { useDispatch, useSelector } from 'react-redux';
 import DeletePostPopup from './DeletePostPopup';
+import { PostType } from '../../Types/otherType';
+import {memo} from 'react'
 
-export default function PostThreeDotModal({ el }: any) {
+interface PostDataType {
+  el:PostType
+}
+
+function PostThreeDotModal({ el }: PostDataType) {
   const [open, setOpen] = useState<boolean>(false);
   const handleOpen = () => setOpen(!open);
   const data = useSelector((store: Initial) => store.localStorageData);
   const authUser: any = useSelector((store: Initial) => store.authUser);
   const dispatch = useDispatch();
+  const postedBy = el.postedBy as AuthUser
 
   const divClass =
     'flex items-center justify-center p-3 border-b-[1px] border-solid border-gray-300 hover:bg-gray-300 rounded-lg text-[15px]';
@@ -27,7 +34,7 @@ export default function PostThreeDotModal({ el }: any) {
             boxShadow:
               'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px',
           }}>
-          {authUser._id === el?.postedBy?._id ? (
+          {authUser._id === postedBy?._id ? (
             <DeletePostPopup id={el?._id} />
           ) : (
             <div
@@ -38,12 +45,6 @@ export default function PostThreeDotModal({ el }: any) {
               <p>Report</p>
             </div>
           )}
-
-          <div
-            className={`${divClass} cursor-not-allowed text-[#f00707]`}
-            onClick={handleOpen}>
-            <p>Unfollow</p>
-          </div>
           <div className={`${divClass} cursor-not-allowed text-black`}>
             <p>Add to favorites</p>
           </div>
@@ -69,3 +70,5 @@ export default function PostThreeDotModal({ el }: any) {
     </>
   );
 }
+
+export default memo(PostThreeDotModal)
